e5cc598074990cc8cf7740d71848afe523553d39
hmilin <hmilin@163.com>
2024-09-20 17:45:48 +0800

    chore: add css compact plugin

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,17 @@ module.exports = {
     "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
+    doiuse: {
+      browsers: [
+        "Chrome >=88",
+        "Firefox >=78",
+        "Safari >=14.1",
+        "Edge >=88",
+        "iOS >=16"
+      ],  // 目标浏览器列表
+      onFeatureUsage: function (usageInfo) {
+        console.log(usageInfo.message);  // 输出不兼容的特性
+      }
+    }
   },
 };
